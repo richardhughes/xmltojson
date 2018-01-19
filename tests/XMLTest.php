3350e9e62a9b28225694b6dcce5dfadc2990c16e
Richie Hughes <eeuc40@hotmail.com>
2018-01-19 21:06:06 +0000

    Add tests

---
+++ tests/XMLTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests;
+
+use Converter\XML;
+use PHPUnit\Framework\TestCase;
+
+class XMLTest extends TestCase
+{
+
+    /**
+     * @dataProvider xmlToArrayDataProvider
+     *
+     * @param string $xml
+     * @param array $expectedOutput
+     */
+    public function testXMLIsFormattedAsArrayCorrectly(string $xml, array $expectedOutput): void
+    {
+        $xmlConverter = new XML(new \SimpleXMLElement($xml), []);
+//        var_dump($xmlConverter->toArray());
+//        exit;
+        $this->assertSame($expectedOutput, $xmlConverter->toArray());
+    }
+
+    public function xmlToArrayDataProvider(): array
+    {
+        return [
+            'Test array is generated correctly without namespaces' => [
+                'xml' => '<?xml version="1.0" encoding="UTF-8"?><root><random>Hello</random><test dep="Attribute">A test</test></root>',
+                'expectedOutput' => [
+                    'root' => [
+                        'random' => 'Hello',
+                        'test' => [
+                            '@dep' => 'Attribute',
+                            '$' => 'A test'
+                        ]
+                    ]
+                ]
+            ]
+        ];
+    }
+
+
+    public function testWorks()
+    {
+        $this->assertTrue(true);
+    }
+}
